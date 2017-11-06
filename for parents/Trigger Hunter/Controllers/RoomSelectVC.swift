a8c1c5e83961d7b476f6981eecf2e27e6832520c
khirani6 <khirani6@gatech.edu>
2017-11-06 09:08:23 -0500

    Setting up CollectionViewController

--- for parents/Trigger Hunter/Controllers/RoomSelectVC.swift
+++ for parents/Trigger Hunter/Controllers/RoomSelectVC.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class RoomSelectVC: UIViewController {
+class RoomSelectVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    let reuseIdentifier = "roomCell"
+    var roomImages : [UIImage] = [UIImage(named:"Outside.png")!, UIImage(named:"Kitchen.png")!, UIImage(named:"Bedroom.png")!, UIImage(named:"Pet.png")!, UIImage(named:"Bathroom.png")!, UIImage(named:"Living_Room.png")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +25,31 @@ class RoomSelectVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return roomImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! RoomCollectionViewCell
+        cell.image.image = self.roomImages[indexPath.item]
+        return cell
+    }
+    
+    /*func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // Compute the dimension of a cell for an NxN layout with space S between
+        // cells.  Take the collection view's width, subtract (N-1)*S points for
+        // the spaces between the cells, and then divide by N to find the final
+        // dimension for the cell's width and height.
+        
+        let cellsAcross: CGFloat = 3
+        let spaceBetweenCells: CGFloat = 1
+        let dim = (collectionView.bounds.width - (cellsAcross - 1) * spaceBetweenCells) / cellsAcross
+        return CGSize(width: dim, height: dim)
+    }*/
+    
+    @IBAction func linkChildDevice(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
