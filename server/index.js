b1305cf82ccfef960efb09f057e87674ee22a0e9
Cal Stephens <cal@calstephens.tech>
2017-12-03 16:11:37 -0500

    Send push notifications in response to a POST /spawnTrigger

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 var apn = require('apn');
+var express = require('express');
+var bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json({limit: "1mb"}))
 
 var options = {
   token: {
@@ -12,11 +17,28 @@ var options = {
 var apnProvider = new apn.Provider(options);
 var deviceToken = "05d5d57f23e6fa9f64c09f38b143a4c396ee7e4b6ab36844221db83aa4336a1c"
 
-var notification = new apn.Notification();
-notification.badge = 1;
-notification.sound = "ping.aiff";
-notification.alert = "Watch out, a dust mite just spawned in your bedroom!";
-notification.payload = {'triggerName': 'Smoke'};
-notification.topic = "edu.gatech.triggerhunter.Trigger-Hunter-AR.cal-stephens";
+app.post('/spawnTrigger', (request, response) => {
+    console.log(request.body)
+    
+    var triggerName = request.body["triggerName"]
+    var alertText = request.body["alertText"]
+    
+    if (triggerName && alertText) {
+        var notification = new apn.Notification();
+        notification.badge = 1;
+        notification.sound = "ping.aiff";
+        notification.alert = alertText;
+        notification.payload = {'triggerName': triggerName};
+        notification.topic = "edu.gatech.triggerhunter.Trigger-Hunter-AR.cal-stephens";
 
-apnProvider.send(notification, deviceToken)
+        apnProvider.send(notification, deviceToken)
+        response.send({'success': true})
+    } else {
+        response.send({'success': false})
+    }
+})
+
+app.listen(8081)
+
+/*
+*/
